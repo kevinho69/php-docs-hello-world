1ae0b5f9acb0a456a51158d3765b9900f09dce36
kevinho69 <136664611+kevinho69@users.noreply.github.com>
2023-06-14 23:55:02 -0300

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
-<?php
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body>
 
-echo "Hello World!";
+<?php include("includes/design-top.php");?>
+<?php include("includes/navigation.php");?>
+
+<div class="container" id="main-content">
+	<h2>BEM VINDOS AO APLICATIVO EM NUVEM</h2>
+	<p>Aplicação desenvolvida e hospedada em nuvem</p>
+
+	<p>
+Você também pode fazer isso é muito fácil.
+	</p>
+	<p>
+e o nosso grupo vai te explicar.	</p>
+</div>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
